981c63ff074dbc22e47a6470b58890d8f40b631c
Pritom Rahaman <pritomrahaman1331@gmail.com>
2022-07-20 22:59:21 +0530

    demo admin panel ui

---
+++ src/formSource.js
@@ -0,0 +1,77 @@
+export const userInputs = [
+    {
+      id: 1,
+      label: "Username",
+      type: "text",
+      placeholder: "john_doe",
+    },
+    {
+      id: 2,
+      label: "Name and surname",
+      type: "text",
+      placeholder: "Piyush Roy",
+    },
+    {
+      id: 3,
+      label: "Email",
+      type: "mail",
+      placeholder: "piyush_roy@gmail.com",
+    },
+    {
+      id: 4,
+      label: "Phone",
+      type: "text",
+      placeholder: "+918025659116",
+    },
+    {
+      id: 5,
+      label: "Password",
+      type: "password",
+    },
+    {
+      id: 6,
+      label: "Address",
+      type: "text",
+      placeholder: "Mukherjee Road,Kolkata-700092",
+    },
+    {
+      id: 7,
+      label: "Country",
+      type: "text",
+      placeholder: "India",
+    },
+  ];
+  
+  export const productInputs = [
+    {
+      id: 1,
+      label: "Title",
+      type: "text",
+      placeholder: "Apple Macbook Pro",
+    },
+    {
+      id: 2,
+      label: "Description",
+      type: "text",
+      placeholder: "Description",
+    },
+    {
+      id: 3,
+      label: "Category",
+      type: "text",
+      placeholder: "Computers",
+    },
+    {
+      id: 4,
+      label: "Price",
+      type: "text",
+      placeholder: "100",
+    },
+    {
+      id: 5,
+      label: "Stock",
+      type: "text",
+      placeholder: "in stock",
+    },
+  ];
+  
